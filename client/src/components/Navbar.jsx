38484ce7a780c5603bb52f0d3cd16d1b3c3d446d
Vishal@gzfs <vismbs@pm.me>
2023-06-24 11:53:06 +0530

    Navbar Updated

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="main_nav__container">
+      <nav className="main_nav">
+        <div className="nav_logo">DeltaTasks</div>
+        <button className="nav_cta__button">Sign Up</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
