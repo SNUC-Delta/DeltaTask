bab25cea76cfd0386e38c1e667dbe2ddca8c5246
Vishal@gzfs <vismbs@pm.me>
2023-06-24 13:05:57 +0530

    Grayscale

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="main_nav__container">
       <nav className="main_nav">
-        <div className="nav_logo">DeltaTasks</div>
+        <div className="nav_logo">Delta Tasks</div>
         <button className="nav_cta__button">Sign Up</button>
       </nav>
     </div>
